92d0533c05fedebed25a741686c66fdb3d1db002
Arelias <robertmusialik95@gmail.com>
2019-05-22 16:44:59 +0200

    Module 6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        if(Calculator.add(5, 10) == (5+10)){
+            System.out.println("addition test OK");
+        } else  {
+            System.out.println("addition test failed");
+        }
+        if(Calculator.subtract(5, 10) == (5-10)){
+            System.out.println("subtraction test OK");
+        } else  {
+            System.out.println("subtraction test failed");
+        }
     }
 }
