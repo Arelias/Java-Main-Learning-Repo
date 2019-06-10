0d9df3d5cc0523947c2aa79931d8bdece2d8f0f1
Arelias <robertmusialik95@gmail.com>
2019-06-10 12:09:22 +0200

    Module 8.4 Exception knowledge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -4,18 +4,33 @@ package com.kodilla.exception.test;
 public class ExceptionHandling {
     public static void main(String args[]){
 
-        SecondChallenge challengeObject = new SecondChallenge();
-        try{
-            System.out.println(challengeObject.probablyIWillThrowException(1, 0));
-            System.out.println(challengeObject.probablyIWillThrowException(2, 1.5));
 
+        Flight flight1 = new Flight("Szymany", "Stansted");
+        Flight flight2 = new Flight("Szymany", "Gdansk");
+        FlightControl controller = new FlightControl();
+        try{
+            controller.findFilght(flight1);
+            controller.findFilght(flight2);
         }
-        catch (Exception e){
-            System.out.println("Arguments have wrong values, " + e);
+        catch (RouteNotFoundException e){
+            System.out.println("Flight not available, " + e);
         }
         finally {
-            System.out.println("Done handling exception");
+            System.out.println("Done searching for flights");
         }
+
 
     }
 }
+//        SecondChallenge challengeObject = new SecondChallenge();
+//        try{
+//            System.out.println(challengeObject.probablyIWillThrowException(1, 0));
+//            System.out.println(challengeObject.probablyIWillThrowException(2, 1.5));
+//
+//        }
+//        catch (Exception e){
+//            System.out.println("Arguments have wrong values, " + e);
+//        }
+//        finally {
+//            System.out.println("Done handling exception");
+//        }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightControl.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightControl {
+
+    public void findFilght(Flight flight) throws RouteNotFoundException, NullPointerException{
+
+        Map<String, Boolean> routes = new HashMap<>();
+        routes.put("Szymany", true);
+        routes.put("Stansted", false);
+        if(!routes.containsKey(flight.arrivalAirport) || !routes.containsKey(flight.departureAirport)){
+            throw new RouteNotFoundException("Route missing");
+        }
+        if(!routes.get(flight.arrivalAirport) || !routes.get(flight.departureAirport)){
+            System.out.println("Route suspended");
+        }
+    }
+}
