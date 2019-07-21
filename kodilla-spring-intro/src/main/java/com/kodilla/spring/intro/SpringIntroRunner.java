352df9aede8e20f47c19913987d29c3a6a5b7b21
Arelias <robertmusialik95@gmail.com>
2019-07-21 19:07:53 +0200

    Module 14.1 forum user

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntroRunner.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.intro;
+
+import com.kodilla.spring.intro.shape.Circle;
+import com.kodilla.spring.intro.shape.Triangle;
+
+public class SpringIntroRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome into the intro to spring");
+
+
+
+    }
+}
