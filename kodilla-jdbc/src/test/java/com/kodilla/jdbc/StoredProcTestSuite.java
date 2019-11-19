623ebf3fafd3229b5761e2981d7194317364b706
Arelias <robertmusialik95@gmail.com>
2019-11-19 12:16:08 +0100

    Module 30.3 Java SQL procedure call

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,61 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        //When updating data use executeUpdate, not executeQuery IMPORTANT
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        //When using stored procedures, use execute IMPORTANT
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0,howMany);
+    }
+
+    //When updating data use executeUpdate, not executeQuery IMPORTANT
+    //When using stored procedures, use execute IMPORTANT
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=TRUE";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertNotEquals(0,howMany);
+    }
+}
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -37,4 +37,25 @@ public class Board {
     public void setDoneList(TaskList doneList) {
         this.doneList = doneList;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board = (Board) o;
+
+        if (toDoList != null ? !toDoList.equals(board.toDoList) : board.toDoList != null) return false;
+        if (inProgressList != null ? !inProgressList.equals(board.inProgressList) : board.inProgressList != null)
+            return false;
+        return doneList != null ? doneList.equals(board.doneList) : board.doneList == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = toDoList != null ? toDoList.hashCode() : 0;
+        result = 31 * result + (inProgressList != null ? inProgressList.hashCode() : 0);
+        result = 31 * result + (doneList != null ? doneList.hashCode() : 0);
+        return result;
+    }
 }
